448238cec472bc7576b94916a8e03ed212030669
Kidist Meseret <kidistmeseret1@gmail.com>
2026-08-13 00:40:58 +0900

    Add assignment starter files

--- Module-02/day19/Assignment/app.js
+++ Module-02/day19/Assignment/app.js
@@ -1,1 +1,26 @@
+// TODO: Hold items in an array (this is your single source of truth)
+let items = [];
 
+// TODO: Select necessary DOM elements (form, input, list, count)
+
+// TODO: Write a render() function to rebuild the list from the array
+// 1. Clear the current list (innerHTML = "")
+// 2. Loop through the items array
+// 3. Create elements, use data-id on each row, and append to the list
+// 4. Update the live count paragraph
+function render() {
+  // Logic goes here...
+}
+
+// TODO: Handle form submission
+// 1. preventDefault to stop page reload
+// 2. Read and validate the input
+// 3. Push a new object to the items array (include a unique id and done: false)
+// 4. Call render()
+
+// TODO: Set up event delegation on the #list
+// 1. Listen for clicks on the parent <ul>
+// 2. Use e.target and closest() to find the clicked row
+// 3. Determine if the user is toggling ".done" or removing a row
+// 4. Update the items array accordingly
+// 5. Call render()
